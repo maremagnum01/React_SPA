6a4804ea0cbac3cf7361dfc7c3ca99e69c8e5855
maremagnum01 <eze.di.lallo@gmail.com>
2025-02-13 15:29:50 -0300

    primer push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Componentes/Header';
+import Nav from './Componentes/Nav';
+import Container from './Componentes/Container';
+import SectionResume from './Componentes/SectionResume';
+import Portfolio from './Componentes/Portfolio';
+import Contact from './Componentes/Contact';
+import Footer from './Componentes/Footer';
+import {ThemeContext} from './Providers/ThemeContext';
+import { useState } from'react';
+import Switcher from './Componentes/Switcher';
+
 
 function App() {
+  const [theme, setTheme] = useState('Light');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className="App" style={{
+          backgroundColor: theme === "Light" ? "#fff" : "#333",
+          color: theme === "Light" ? "#000" : "#ccc",
+        }}>
+        <Header/>
+        <Nav/>
+        {/* <Switcher/> */}
+        <Container/>
+        <SectionResume/>
+        <Portfolio/>
+        <Contact/>
+        <Footer/>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/Componentes/Footer.jsx
@@ -0,0 +1,12 @@
+
+export default function Footer (){
+    return (
+        <footer class="footer py-3">
+        <div class="container">
+            <p class="small mb-0 text-light">
+                &copy; <script>document.write(new Date().getFullYear())</script>  <i class="ti-heart text-danger"></i>
+            </p>
+        </div>
+    </footer>
+    )
+}

c0dbf10fb357d0752361fad4e7434a8c5c2df0aa
maremagnum01 <eze.di.lallo@gmail.com>
2025-03-01 14:55:56 -0300

    se arreglo el link del proyecto gestion de empresa

--- src/Componentes/Portfolio.jsx
+++ src/Componentes/Portfolio.jsx
@@ -15,8 +15,8 @@ export default function Portfolio() {
                                 <img class="card-img-top" src="imagenes/Gestion_de_empresa.jpg" alt="Empresa..."></img>
                                     <div class="card-body">
                                         <h5 class="card-title">Gestion de Empresa</h5>
-                                        <p class="card-text">Es un CRUD realizado con php puro, html, bootstrap y jquery. Función realizar registros de empleados, realizar registro de usuario para manejar dichos registros, tiene un buscador en tiempo real realizado con AJAX.</p>
-                                        <a href="http://www.crudphp.infinityfreeapp.com" target="_blank" class="btn btn-primary">Ir al sitio</a>
+                                        <p class="card-text">Es un CRUD realizado con php puro, html, bootstrap y jquery. Función realizar registros de empleados, realizar registro de usuario para manejar dichos registros, tiene un buscador en tiempo real realizado con AJAX. Utiliza una base de datos SQL con MySQL.</p>
+                                        <a href="http://www.gestionempresa-crud.infinityfreeapp.com/" target="_blank" class="btn btn-primary">Ir al sitio</a>
                                     </div>
                             </div>
                         </div>
